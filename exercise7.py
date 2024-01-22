214eb1ec3862cc84697a0f59179dc65eedf43bd7
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-22 15:18:37 +0800

    Finished Code!

--- exercise7.py
+++ exercise7.py
@@ -1,3 +1,9 @@
 str_x = "Emma is good developer. Emma is a writer"
 # split the string by spaces to a list
+converted_str_to_list = str_x.split(' ')
 # find how many Emma appears on the list 
+emma_occurence = converted_str_to_list.count('Emma')
+print(f'Emma appeared {emma_occurence} time{'s' if emma_occurence > 1 else ''}')
+
+# Output:
+# Emma appeared 2 times
